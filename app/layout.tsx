c71aeb5589f7d360b4ebca888e357fadd3374497
Daniel-627 <ochiengdaniel627@gmail.com>
2025-01-14 00:57:15 +0300

    Changing color

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased mx-auto max-w-[1440px] bg-[#570033]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased mx-auto max-w-[1440px] bg-[#1f0f53]`}
       >
         <Navbar />
         {children}

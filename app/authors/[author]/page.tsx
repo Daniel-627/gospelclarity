ec825a1ce4672de654b39b45985113764656eda2
Daniel-627 <ochiengdaniel627@gmail.com>
2025-02-25 16:27:02 +0300

    author slug

--- app/authors/[author]/page.tsx
+++ app/authors/[author]/page.tsx
@@ -31,7 +31,7 @@ export default async function AuthorPage({ params }: AuthorPageProps) {
         {posts.map((post) => (
           <li key={post._id}>
             {/* Ensure post.slug.current is properly accessed */}
-            <Link href={`/trial/${post.slug.current}`}>
+            <Link href={`/blog/${post.slug.current}`}>
               <div className="bg-gray-100 p-4 rounded-lg hover:bg-gray-200 transition cursor-pointer">
                 <h2 className="text-xl font-semibold">{post.title}</h2>
                 <p className="text-gray-700 mt-2">

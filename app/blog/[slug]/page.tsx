efe06ff05200d754d2a8b341c9ca021093d0201a
Daniel-627 <ochiengdaniel627@gmail.com>
2025-03-12 14:06:24 +0300

    mt

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -44,7 +44,7 @@ export default async function Page({ params }: PageProps) {
   }
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 mt-16">
       {/* Title */}
       <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
 

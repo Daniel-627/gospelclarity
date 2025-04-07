e134f7337e53bcb5f176c322140d4172854d0b00
Daniel-627 <ochiengdaniel627@gmail.com>
2025-04-07 16:58:21 +0300

    flex

--- components/TriList.tsx
+++ components/TriList.tsx
@@ -7,10 +7,10 @@ type Props = {}
 
 export default function TriList({}: Props) {
   return (
-    <div className='flex flex-col md:flex-row space-x-2 p-4 border-b-2 w-full'>
-        <div><ApologeticsBlogList/></div>
-        <div><BibleStudiesBlogList/></div>
-        <div><SermonsBlogList/></div>
+    <div className='flex flex-col md:flex-row p-4 border-b-2 w-full'>
+        <div className="w-full md:flex-1"><ApologeticsBlogList/></div>
+        <div className="w-full md:flex-1"><BibleStudiesBlogList/></div>
+        <div className="w-full md:flex-1"><SermonsBlogList/></div>
     </div>
   )
 }

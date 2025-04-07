7f0b59146e5ee674870d31368b1d0f3bb0c5f83d
Daniel-627 <ochiengdaniel627@gmail.com>
2025-04-07 17:01:28 +0300

    gap

--- components/TriList.tsx
+++ components/TriList.tsx
@@ -7,7 +7,7 @@ type Props = {}
 
 export default function TriList({}: Props) {
   return (
-    <div className='flex flex-col md:flex-row p-4 border-b-2 w-full'>
+    <div className='flex flex-col md:flex-row p-4 gap-4 border-b-2 w-full'>
         <div className="w-full md:flex-1"><ApologeticsBlogList/></div>
         <div className="w-full md:flex-1"><BibleStudiesBlogList/></div>
         <div className="w-full md:flex-1"><SermonsBlogList/></div>
